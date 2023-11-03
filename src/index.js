3eeabb257db390628d3ff95e109f5db478fd18db
Maksimdon1 <maksimkozyrev056@gmail.com>
2023-11-04 00:49:19 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-// import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import store from './store/store'
+import { Provider } from 'react-redux'
 import { BrowserRouter} from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { store } from './store/index.js';
+
 
 
 const queryClient = new QueryClient();
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode> 
+ReactDOM.render(
+	<Provider store={store}>
+		    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-      
-         <App />
-      </Provider>
-      </QueryClientProvider>
-    </BrowserRouter>
-  
-     </React.StrictMode> 
-);
+			<App />
+	</BrowserRouter>
+	</QueryClientProvider>
+	</Provider>,
+
+	document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
