bcb8e7a2d5098f95a20e24d567398370b487e49a
Maksimdon1 <maksimkozyrev056@gmail.com>
2023-11-04 00:52:19 +0300

    mmm

--- src/components/TEST/src/components/right_panel.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export  function Right_panel() {
-  return (
-    <div>right_panel</div>
-  )
-}
--- src/components/TEST/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit" 
-import { reducer } from "./user/user.slice"
-
-export const store = configureStore({
-   reducer,
-
-})
