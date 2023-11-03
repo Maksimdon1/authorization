a99143ade582eacb7429aea1b2a6ca9ba1e20c6e
Maksimdon1 <maksimkozyrev056@gmail.com>
2023-11-04 02:09:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 import store from './store/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter} from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Provider store={store}>
-		    {/* <QueryClientProvider client={queryClient}> */}
+  <React.StrictMode> 
     <BrowserRouter>
-			<App />
-	</BrowserRouter>
-	{/* </QueryClientProvider> */}
-	</Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+      
+         <App />
+      </Provider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  
+     </React.StrictMode> 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
