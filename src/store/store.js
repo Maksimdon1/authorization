64f163b61e46b8ab539bf5d692e460c911f39e7f
Maksimdon1 <maksimkozyrev056@gmail.com>
2023-11-04 13:23:44 +0300

    frfrfrfrf

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,16 @@
-import { applyMiddleware, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import {  createStore } from 'redux'
+
 import thunk from 'redux-thunk'
 
 import rootReducer from './combainer'
 
 const initialState = {}
-const middlewares = [thunk]
+
 
 const store = createStore(
 	rootReducer,
 	initialState,
-	composeWithDevTools(applyMiddleware(...middlewares))
+	
 	// process.env.APP_ENV === 'development'
 	// 	? composeWithDevTools(applyMiddleware(...middlewares))
 	// 	: applyMiddleware(...middlewares)
